0415a9f03db171cad7fe42e2e10d8da914bf0287
Maja021 <majacekic90@gmail.com>
2022-03-16 11:02:52 +0100

    Add board, Org

--- cypress/integration/createBoard.spec.js
+++ cypress/integration/createBoard.spec.js
@@ -1,29 +1,29 @@
-import { createOrg } from '../page_objects/createOrgPOM';
+import { createBoard } from '../page_objects/createBoardPOM';
 import { loginPage } from '../page_objects/loginPOM';
 
-describe("create org test", () => {
+describe("create board test", () => {
 
   beforeEach("visit board", () => {
     cy.visit("/login");
     loginPage.loginUser('test1@gmail.com', 'test123');
-    cy.wait(1000)
     cy.get('.vs-l-sidebar').should('have.css', 'background-color', 'rgb(41, 41, 41)')
     cy.url().should('contain', '/my-organizations')
   });
 
-  it('create Organization', () => {
+  it.only('create Board', () => {
+
     cy.intercept({
       method: "POST",
-      url: "https://cypress-api.vivifyscrum-stage.com/api/v2/organizations",
-    }).as('organizationRequest')
-    createOrg.hoverOver.click({ force: true })
-    createOrg.addOrg.click()
-    createOrg.addOrganization("test1");
-    cy.wait('@organizationRequest').then((interceptObj) => {
+      url: " https://cypress-api.vivifyscrum-stage.com/api/v2/boards",
+    }).as('createBoardRequest')
+    createBoard.createNewBoard('titleTest')
+    cy.wait('@createBoardRequest').then((interceptObj) => {
       console.log('OBJ', interceptObj)
       expect(interceptObj.response.statusCode).eq(201)
-      expect(interceptObj.response.body.name).eq("test1")
+      expect(interceptObj.response.body.name).eq('titleTest')
     })
     cy.get('span').should('contain', 'test1')
-  });
+    cy.get('.vs-c-list__btn').should('contain', 'titleTest')
+
+  })
 })
--- cypress/page_objects/createOrgPOM.js
+++ cypress/page_objects/createOrgPOM.js
@@ -1,8 +1,18 @@
 
-export default class CreateOrg{
+export default class CreateOrg {
 
-    get hoverOver() { return cy.get(".vs-c-list__btn").eq(1)}
-    get addOrg()  { return cy.get("span").contains('Add Organization')}
-    get orgName()  { return cy.get("input")}
+    get hoverOver() { return cy.get(".vs-c-list__btn").eq(1) }
+    get addOrg() { return cy.get("span").contains('Add Organization') }
+    get orgName() { return cy.get("input") }
+    get btnNext() { return cy.get(".el-button").eq(3) }
+    get btnCreate() { return cy.get(".el-button").contains('Create') }
+    get btnOK() { return cy.get(".vs-c-modal--features-button").contains('OK') }
+    
+    addOrganization(orgName) {
+        this.orgName.type(orgName)
+        createOrg.btnNext.click()
+        createOrg.btnCreate.click()
+        createOrg.btnOK.click()
+    }
 }
 export const createOrg = new CreateOrg();
